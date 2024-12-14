a8aa313c96c05ffeb7120c5c8322efcd1409dcc2
SB Designs <hello@trackle.serv00.net>
2024-12-14 08:08:03 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Share Button
+const shareButton = document.getElementById("shareButton");
+
+      shareButton.addEventListener("click", async () => {
+        if (navigator.share) {
+          try {
+            await navigator.share({
+              title: "Trackle",
+              text: "Play Trackle by SB Designs today and attempt to guess all of the stations on TfL and National Rail! Play now and have fun at https://trackle.pages.dev!",
+            });
+            console.log("Trackle shared successfully!");
+          } catch (error) {
+            console.error("Error sharing Trackle:", error);
+          }
+        } else {
+          alert(
+            "Sharing is not supported here."
+          );
+        }
+      });
