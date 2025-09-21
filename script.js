913a61fc9349ade39ecd974aa81181d7f13c5a92
SB Designs <hello@sbdesigns.spike.team>
2025-09-21 18:55:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ shareButton.addEventListener("click", async () => {
         try {
             await navigator.share({
                 title: "Trackle",
-                text: "Play Trackle by SB Designs today and attempt to guess all of the stations on TfL! Play now at https://trackle.pages.dev!",
+                text: "Play Trackle by SB Designs today and attempt to guess UK railway stations! Play now at https://trackle.pages.dev!",
             });
             console.log("Trackle shared successfully!");
         } catch (error) {
             console.error("Error sharing Trackle:", error);
         }
-    } else {
+    } else {s
         alert("Sharing is not supported here.");
     }
 });
